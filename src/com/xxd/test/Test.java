77f3c790e2c9d9cbec454db466f003c8e32ea7ba
bigheary <xie.xd@foxmail.com>
2016-10-26 19:31:34 +0800

    10-26-19:31

--- src/com/xxd/test/Test.java
+++ src/com/xxd/test/Test.java
@@ -7,5 +7,6 @@ public class Test {
     public static void main(String[] args){
         System.out.println("hello world!");
         System.out.println("changed by computer!");
+        System.out.println("change 2 time!");
     }
 }
